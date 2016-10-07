f1079d9130c432639e9b0d22b41f55bbe9457e6b
Eunice Heymann <heymann.eunice@gmail.com>
2016-10-07 13:40:17 -0500

    Created inline code for a while loop that uses console.log() to create the output shown below.

--- js/While_Loop.js
+++ js/While_Loop.js
@@ -2,18 +2,20 @@
 
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
-// This is how you get a random number between 1 and 5
+
 
 
 do {
+    // This is how you get a random number between 1 and 5
     var cones = Math.floor(Math.random() * 5) + 1;
     if (allCones >= cones) {
-        console.log("Customer bought " + cones + "/ " + allCones + " cones!");
-        allCones = allCones - cones; //can also code allCones -= cones
+        console.log("Customer bought " + cones + "/" + allCones + " cones!");
+        allCones = allCones - cones;
+        //can also code allCones -= cones
     } else  {
         console.log("Cannot sell you " + cones + " I only have " + allCones + " ...");
     }
 } while (allCones !== 0);
-console.log("Yay! I sold them all!");
+console.log("Yay! I sold all of them!");
 
 
